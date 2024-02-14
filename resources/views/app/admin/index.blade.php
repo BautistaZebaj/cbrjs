c22d8f87fc63b9f6d33cde5263f312a600bf7f4a
BautistaZebaj <130525525+BautistaZebaj@users.noreply.github.com>
2024-02-14 11:57:04 +0800

    Initial commit

---
+++ resources/views/app/admin/index.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title', 'Jabez\'s Blog')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('footer')
+Copyright &copy 2023, Jabez Bautista. All rights reserved
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
